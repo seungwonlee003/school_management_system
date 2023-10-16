b2e7481ea0d03c1914c089d12deb8719287e85b6
seungwonlee003 <seungwonlee03@icloud.com>
2023-10-16 19:49:54 +0900

    fixed bugs

--- src/test/java/com/example/school_management_system/repository/AbstractContainerBaseTest.java
+++ src/test/java/com/example/school_management_system/repository/AbstractContainerBaseTest.java
@@ -1,13 +1,21 @@
 package com.example.school_management_system.repository;
 
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 
-public class AbstractContainerBaseTest {
-    static final MySQLContainer MY_SQL_CONTAINER;
+public abstract class AbstractContainerBaseTest {
+    private static final MySQLContainer container = (MySQLContainer) new MySQLContainer("mysql:8.0.26");
 
     static {
-        MY_SQL_CONTAINER = new MySQLContainer("mysql:8.1.0");
-        MY_SQL_CONTAINER.start();
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
     }
 
 }
--- src/test/java/com/example/school_management_system/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/school_management_system/repository/StudentRepositoryTest.java
@@ -1,15 +1,21 @@
 package com.example.school_management_system.repository;
 
-import com.example.school_management_system.dto.StudentRequest;
 import com.example.school_management_system.model.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.List;
+
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Slf4j
 public class StudentRepositoryTest extends AbstractContainerBaseTest{
     @Autowired
     private StudentRepository studentRepository;
@@ -18,6 +24,31 @@ public class StudentRepositoryTest extends AbstractContainerBaseTest{
     void shouldSaveStudent(){
         Student expectedStudent  = new Student(1L, "Seungwon Lee", 12, null);
         Student actualStudent = studentRepository.save(expectedStudent);
+        Assertions.assertThat(actualStudent)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(expectedStudent);
+    }
+
+    @Test
+    void shouldGetAllStudents() {
+        Student student1 = new Student(2L, "Kung Lee", 12, null);
+        Student student2 = new Student(3L, "Jungwon Lee", 12, null);
+        Student student3 = new Student(4L, "Jungkok Lee", 12, null);
+        studentRepository.save(student1);
+        studentRepository.save(student2);
+        studentRepository.save(student3);
+
+        List<Student> expectedStudents = Arrays.asList(student1, student2, student3);
+        List<Student> actualStudents = studentRepository.findAll();
+        Assertions.assertThat(expectedStudents.size()).isEqualTo(actualStudents.size());
+    }
+
+    @Test
+    void shouldGetStudent(){
+        Student expectedStudent = new Student(1L, "Seungwon Lee", 12, null);
+        Student student = studentRepository.save(expectedStudent);
+        Student actualStudent = studentRepository.findById(student.getId()).orElse(null);
         Assertions.assertThat(actualStudent)
                 .usingRecursiveComparison()
                 .ignoringFields("id")
